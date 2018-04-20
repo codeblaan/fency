5760b4dfc877da202c52eba789d6aa30a71ded69
blaan <blaan@protonmail.com>
2018-04-19 18:59:30 -0700

    Add release script

---
+++ release.sh
@@ -0,0 +1,26 @@
+VERSION=$1
+NAME=Fency
+DIR=$NAME-$VERSION
+json -I -f package.json -e "this.version='${VERSION}'"
+mkdir -p release-builds/$DIR
+electron-packager . \
+  --overwrite \
+  --platform=darwin \
+  --icon=assets/icon.icns \
+  --arch=x64 \
+  --prune=true \
+  --out=release-builds
+rm -rf release-builds/$DIR/
+rm -rf release-builds/$DIR.dmg
+rm -rf release-builds/$DIR.dmg.zip
+mkdir -p release-builds/$DIR/
+cp -R release-builds/Fency-darwin-x64/Fency.app release-builds/$DIR/Fency.app
+echo "Drag Fency.app to Applications folder to install" > release-builds/$DIR/Notes.txt
+echo "@codeblaan at github" >> release-builds/$DIR/Notes.txt
+ln -s /Applications release-builds/$DIR/Applications
+hdiutil create \
+  -fs HFS+ \
+  -srcfolder release-builds/$DIR/ \
+  release-builds/$DIR.dmg
+zip -r release-builds/$DIR.dmg.zip release-builds/$DIR.dmg
+open release-builds/
